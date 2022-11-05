656b130bd4b5c4fb859e2c300c14920e92596bd4
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-05 23:32:54 +0700

    lecture 28

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Le Minh Nghia'),
+        ),
+        body: Text('This is my default text!'),
+      ),
     );
   }
 }

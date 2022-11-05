414b7b2cc3a9684b656cd44dbe283fa6307646d6
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-05 23:56:51 +0700

    lecture 30

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,27 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your name?', 'How are u?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Le Minh Nghia'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }

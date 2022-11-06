f622f3d96e00be6bb6b44a34991da2eb7da9c6c4
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-06 09:19:01 +0700

    lecture 33

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,14 @@ class MyApp extends StatelessWidget {
           ),
           ElevatedButton(
             child: Text('Answer 2'),
-            onPressed: answerQuestion,
+            onPressed: () => print('Answer 2 chosen!'),
           ),
           ElevatedButton(
             child: Text('Answer 3'),
-            onPressed: answerQuestion,
+            onPressed: () {
+              // ...
+              print('Answer 3 chosen!');
+            },
           ),
         ]),
       ),

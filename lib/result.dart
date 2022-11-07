f20227107efbd34f7b2e30cf73036bc63599f333
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-07 17:11:05 +0700

    lecture 55

--- lib/result.dart
+++ lib/result.dart
@@ -6,8 +6,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = "You did it!";
@@ -26,13 +27,21 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text('Restart Quiz!'),
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
